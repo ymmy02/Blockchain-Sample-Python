2ba56632d8a0069e0aa4d3868697d792648f701e
Kohei Yamamoto <kh2yam1@gmial.com>
2019-08-22 12:26:52 -0400

    Implement new_transaction

--- blockchain.py
+++ blockchain.py
@@ -6,8 +6,20 @@ class Blockchain(object):
     def new_block(self):
         pass
     
-    def new_transaction(self):
-        pass
+    def new_transaction(self, sender: str, recipient: str, amount: int) -> int:
+        """
+        :param sender: Address of Sender
+        :param recipient: Address of Recipient
+        :param amount: Amount
+        :return: Address of Block which includes this Transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
