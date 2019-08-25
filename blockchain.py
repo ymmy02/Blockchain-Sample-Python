ce341edf81e9f0d15c8cd853f36dc2c4b1f8e48f
Kohei Yamamoto <kh2yam1@gmial.com>
2019-08-25 11:10:09 -0400

    Implement new_block

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,29 @@
+from typing import Dict
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+    def new_block(self, proof: int, previous_hash: str=None) -> Dict:
+        """
+        :param proof: Proof from the proof of work algorithm
+        :param previous_hash: (Option) Hash of previous hash
+        :return: New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
     
     def new_transaction(self, sender: str, recipient: str, amount: int) -> int:
         """
