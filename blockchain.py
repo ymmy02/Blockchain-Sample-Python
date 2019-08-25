394a189e99ce590297fe3c5b8b1c2aff1c4776a6
Kohei Yamamoto <kh2yam1@gmial.com>
2019-08-25 11:52:34 -0400

    Implement proof of work

--- blockchain.py
+++ blockchain.py
@@ -53,3 +53,19 @@ class Blockchain(object):
     @property
     def last_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof: int) -> int:
+        """
+        Simple algorithm of the proof of work
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof: int, proof: int) -> bool:
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
